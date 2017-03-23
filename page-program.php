4dcb63d1433543f77c35b427da0e12d59277f489
Daniel WIllson <daniel@danielwillson.me>
2017-03-23 18:32:29 -0400

    Added in empty program page

---
+++ page-program.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Program Page Template
+ *
+ * @package WordPress
+ * @subpackage Starkers
+ * @since Starkers HTML5 3.0
+ */
+
+get_header(); ?>
+
+	<?php //get_template_part( 'loop', 'page' ); ?>
+
+	<!-- JULIUS TOP CODE HERE -->
+
+
+	<!-- FELIX CODE HERE -->
+
+
+	<!-- KATIE CODE HERE -->
+
+	<!-- JULIUS CODE HERE -->
+
+	<!-- BRANDON CODE HERE -->
+
+	<!-- CHRIS CODE HERE -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
