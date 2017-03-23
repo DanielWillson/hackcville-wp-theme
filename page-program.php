8313505e28f710fa97c005c0acb815140d3b85ba
juliusha36 <juliusha36@gmail.com>
2017-03-23 18:37:46 -0400

    created top and testimonial section of program page

--- page-program.php
+++ page-program.php
@@ -12,7 +12,11 @@ get_header(); ?>
 	<?php //get_template_part( 'loop', 'page' ); ?>
 
 	<!-- JULIUS TOP CODE HERE -->
+	<div class="Title"><h1>Hustle</h1><h3>HackCville's Entrepreneurship Program</h3></div>
 
+<svg width="70" height="55" viewBox="-2.5 -5 75 60" preserveAspectRatio="none" >
+  <path d="M0,0 l35,50 l35,-50" fill="none" stroke="white" stroke-linecap="round" stroke-width="5" />
+</svg>
 
 	<!-- FELIX CODE HERE -->
 
@@ -20,6 +24,14 @@ get_header(); ?>
 	<!-- KATIE CODE HERE -->
 
 	<!-- JULIUS CODE HERE -->
+		
+	<div class="instructor">
+
+		<div class="instructorphoto"><img src="dw-prof1.jpg" /></div>
+		<div class="blurb"><h4>Daniel Willson</h4><h5>Lorem Ipsum Dolororem Ipsum Dolororemrorem Ipsum Drorem Ipsum Drorem Ipsum Drorem Ipsum Drorem Ipsum Drorem Ipsum Drorem Ipsum Drorem Ipsum Drorem Ipsum D Ipum Dolororem Ipsum Dolororem Ipsum Dolororem Ipsum Dolor Ipsum Dolororem Ipsum Dolor</h5></div> 
+
+	</div>
+
 
 	<!-- BRANDON CODE HERE -->
 
