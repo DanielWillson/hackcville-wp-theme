0c6a90ce8b3fc18d85707570d63360c4f81deaea
Brandon Liu <liu.b97@gmail.com>
2017-03-23 18:49:35 -0400

    Added student outcomes section

--- page-program.php
+++ page-program.php
@@ -83,6 +83,45 @@ get_header(); ?>
 
 	<!-- BRANDON CODE HERE -->
 
+	<div class="container">
+		<div class="title">
+			<h1>Student Outcomes</h1>
+		</div> 
+		<div class="content">
+			<div class="outcome">
+				<div class="image">
+					<img src="images/uva.jpg">
+				</div>
+				<div class="caption">
+					<h1>Title</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+
+			<div class="outcome">
+				<div class="image">
+					<img src="images/uva.jpg">
+				</div>
+				<div class="caption">
+					<h1>Title</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+
+			<div class="outcome">
+				<div class="image">
+					<img src="images/uva.jpg">					
+				</div>
+				<div class="caption">
+					<h1>Title</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				</div>
+			</div>
+
+		</div>
+	</div>
+
 	<!-- CHRIS CODE HERE -->
 <div class="bg">
 	</div>
